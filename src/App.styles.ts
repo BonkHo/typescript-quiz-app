5a83f041e6c4b9e55b87101ee9806bb1ec38583e
John <John@DESKTOP-SRO1BKN>
2021-08-26 20:49:03 -0700

    Functionality of the app completed, installed Styled-Components

---
+++ src/App.styles.ts
@@ -0,0 +1,62 @@
+import styled, { createGlobalStyle } from "styled-components";
+import BGImage from "./images/img.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        font-family: 'Noto Sans JP', sans-serif;
+    }
+
+    html {
+        height: 100%;
+    }
+
+    body {
+        /* background-image: url(${BGImage}); */
+        background-size: cover;
+        margin: 0;
+        padding: 0 20px;
+        display: flex;
+        justify-content: center;
+    }
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > p {
+        color: #fff;
+    }
+
+    .score {
+        color: #fff;
+        font-size: 2rem;
+        margin: 0;
+    }
+
+    .h1 {
+        color: #fff;
+        font-family: "Noto Sans JP", sans-serif;
+        -moz-text-fill-color: transparent;
+        font-size: 70px;
+        font-weight: 400;
+        text-align: center;
+    }
+
+    .start,
+    .next {
+        cursor: pointer;
+        background: linear-gradient(180deg, #fff, #00ffb3);
+        border: 2px solid black;
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+        border-radius: 10px;
+        height: 35px;
+        margin: 1em 0;
+    }
+
+    .start {
+        max-width
+    }
+`;
